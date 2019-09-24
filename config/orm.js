6dd5d05d1fd98c4b249f599bb075b5965308075c
TabooThao <daniel.ntaj.thao@gmail.com>
2019-09-24 17:05:25 -0500

    added a lot of things tht I'm not sure where to look, its a deep jungle

---
+++ config/orm.js
@@ -0,0 +1,24 @@
+// Import MySQL connection.
+const connection = require("./connection.js");
+
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+
+// Object for all our SQL statement functions.
+const orm = {
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,27 @@
+INSERT INTO friends (name, photo, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10)
+VALUES 
+("John", 
+"https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=592&q=80",
+3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+);
+
+INSERT INTO friends (name, photo, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10)
+VALUES 
+("Mary", 
+"https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80",
+2, 4, 5, 5, 5, 4, 3, 1, 3, 2
+);
+
+INSERT INTO friends (name, photo, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10)
+VALUES 
+("Sadoko", 
+"https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
+5, 4, 2, 3, 3, 1, 5, 4, 4, 2
+);
+
+INSERT INTO friends (name, photo, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10)
+VALUES 
+("Helen", 
+"https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=562&q=80",
+4, 1, 1, 4, 4, 2, 1, 2, 1, 5
+);
